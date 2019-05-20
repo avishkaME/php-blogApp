85d68ea4505c756fdc500465e45a3f64628863ea
avishkaME <ameddegoda143@gmail.com>
2019-05-20 13:33:39 +0530

    displaying post on the blog

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
 <?php
-  require('config/db.php');
+require('config/db.php');
 
-  //create query
-  $query = SELECT * FROM posts;
+//create query
+$query = 'SELECT * FROM posts';
 
-  //get result
-  $result = mysqli_query($conn, $query);
+//get result
+$result = mysqli_query($conn, $query);
 
-  //fetch data
-  $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//fetch data
+$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-  //free result
-  mysqli_free_result($result);
+//free result
+mysqli_free_result($result);
 
-  //close connection
-  mysqli_close($conn);
+//close connection
+mysqli_close($conn);
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>my Blog</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Posts</h1>
+    <?php foreach ($posts as $post) : ?>
+      <div class="well">
+        <h3><?php echo $post['title']; ?> </h3>
+        <small>Creared on <?php echo $post['created_at']; ?> by <?php echo $post['writer']; ?></small>
+        <p><?php echo $post['body']; ?></p>
+      </div>
+
+    <?php endforeach; ?>
+  </div>
+
+
+</body>
+</html>
