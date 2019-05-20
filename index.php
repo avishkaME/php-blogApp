f624f54b58979a1b108ce936155f9ba999d6045b
avishkaME <ameddegoda143@gmail.com>
2019-05-20 13:32:36 +0530

    fetching data from database

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
   require('config/db.php');
+
+  //create query
+  $query = SELECT * FROM posts;
+
+  //get result
+  $result = mysqli_query($conn, $query);
+
+  //fetch data
+  $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  //free result
+  mysqli_free_result($result);
+
+  //close connection
+  mysqli_close($conn);
